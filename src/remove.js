ad7b5bdd4f36aceb2afb517e19c3f1721cc1cb7e
Roger Stach <rogerdanistach@gmail.com>
2018-01-08 22:22:56 +0700

    The remove posts feature is done.

--- src/remove.js
+++ src/remove.js
@@ -1,20 +1,23 @@
-let ref = firebase.database().ref('rootRef/posts')
+let postsRef = firebase.database().ref('rootRef/posts')
 
 function createArticle(title, date, key, text) {
 	const root = $('#postsSection');
 
-	let str = `<section class="hero is-dark is-bold is-small promo-block">
+	let str = `<section class="hero is-dark is-bold is-small promo-block" id="${key}">
 			<div class="hero-body">
 			<div class="container">
-			<h1 class="title"> 
-			<a href=post.html?postId=${key}>
+			<h1 class="title">
 			${title}
-			</a>
-			<i class="fa fa-fire"></i>
 			</h1>
 			<h2 class="subtitle">
 			Written on ${date}
 			</h2>
+			<br>
+			<h1 class="title">
+			<a class="button is-danger" href="javascript:removePost('${key}')">
+			Remove Post
+			</a>
+			</h1>
 			</div>
 			</div>
 			</section>`
@@ -22,11 +25,16 @@ function createArticle(title, date, key, text) {
 	root.prepend(str)
 }
 
-ref.once('value', function(snapshot) {
+postsRef.once('value', function(snapshot) {
 	$('#loadingBar').addClass('is-hidden');
 	
 	snapshot.forEach(function(child) {
 		createArticle(child.val().title, child.val().date, child.key, child.val().content)
-		// console.log(child.val())
 	})
 })
+
+function removePost(key) {
+	let deletedPost = $('#' + `${key}`)
+	deletedPost.addClass('is-hidden')
+	return postsRef.child(key).remove()
+}
